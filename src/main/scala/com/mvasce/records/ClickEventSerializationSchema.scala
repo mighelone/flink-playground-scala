a76360f5c8ca3c3a507a8d0a340f97c1183d22ad
Michele Vascellari <michele.vascellari@gmail.com>
2020-02-23 22:00:44 +0000

    Add producer

--- src/main/scala/com/mvasce/records/ClickEventSerializationSchema.scala
+++ src/main/scala/com/mvasce/records/ClickEventSerializationSchema.scala
@@ -1,14 +1,16 @@
 package com.mvasce.records
 
-import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema
+// import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema
+import org.apache.kafka.common.serialization.Serializer
 import org.apache.kafka.clients.producer.ProducerRecord
 import play.api.libs.json.Format
 import org.joda.time.DateTime
 import play.api.libs.json.JodaReads
 import play.api.libs.json.JodaWrites
 import play.api.libs.json.Json
+import java.util
 
-class ClickEventSerializationSchema(val topic: String) extends KafkaSerializationSchema[ClickEvent]{
+class ClickEventSerializationSchema extends Serializer[ClickEvent]{
     val pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
     implicit val dateFormat = Format[DateTime](
         JodaReads.jodaDateReads(pattern),
@@ -16,7 +18,11 @@ class ClickEventSerializationSchema(val topic: String) extends KafkaSerializatio
     )
     implicit val clickEventReader = Json.format[ClickEvent]
 
-    override def serialize(element: ClickEvent, timestamp: java.lang.Long): ProducerRecord[Array[Byte],Array[Byte]] = {
-        new ProducerRecord(topic, Json.toJson(element).toString().getBytes())
+    override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
+
+    override def serialize(topic: String, element: ClickEvent): Array[Byte] = {
+        Json.toJson(element).toString().getBytes()
     }
+
+    override def close(): Unit = {}
 }
